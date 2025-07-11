e7c3b4cb73d37c4be7a9d1f9cb0b65e0d7b209a6
Soumya Dey <soumyadey@example.com>
2025-07-11 17:44:11 +0530

    key voult code change 2

--- azurerm_key_vault/main.tf
+++ azurerm_key_vault/main.tf
@@ -6,4 +6,11 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
 }
-
+resource "azurerm_key_vault" "kv1" {
+  name                        = "bhawesh-keyvault"  
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = "bhawesh-keyvault-rg"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+}
